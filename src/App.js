931fd8ada6871470b9ca2be1cbb9ad44835ed067
Lokteeva <swsw06@mail.ru>
2023-09-26 15:41:06 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+// import HomePage from 'pages/HomePage';
+// import LoginPage from 'pages/LoginPage';
+// import RegisterPage from 'pages/RegisterPage';
+// import {Routes, Route} from 'react-router-dom';
+// import { db } from './firebase';
+// import {
+//   query,
+//   collection,
+//   onSnapshot,
+//   updateDoc,
+//   doc,
+//   addDoc,
+//   deleteDoc,
+// } from 'firebase/firestore';
+
+
+function App() {
+  return (
+  <div>
+    lalala
+  </div>
+  );
+}
+
+export default App;
